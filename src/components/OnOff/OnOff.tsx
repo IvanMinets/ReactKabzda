74fa6e3bc47575bf23fd4bca0463e761bcaa4490
Ivan Minets2 <minetssmash@gmail.com>
2023-05-19 12:27:35 +0300

    Added OnOff component

---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type PropsType = {
+    on: boolean
+}
+
+const onStyle = { };
+const offStyle = { };
+const indicatorStyle = {
+    width: "30px",
+    height: "30px",
+    borderRadius: "15px"
+};
+
+function OnOff(props: PropsType) {
+    return <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+}
+
+export default OnOff
