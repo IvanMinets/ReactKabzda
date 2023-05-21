9595971c06dfd292c347e93ad181d180f02ace87
Ivan Minets <minetssmash@gmail.com>
2023-05-21 22:11:35 +0300

    Update OnOff.tsx

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -8,18 +8,28 @@ function OnOff(props: PropsType) {
     const onStyle = {
         width: "30px",
         height: "20px",
-        border: "1px solid black"
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: props.on ? "green" : "white"
     };
     const offStyle = {
         width: "30px",
         height: "20px",
-        border: "1px solid black"
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "2px",
+        padding: "2px",
+        backgroundColor: props.on ? "white" : "red"
     };
     const indicatorStyle = {
-        width: "30px",
-        height: "30px",
+        width: "10px",
+        height: "10px",
         borderRadius: "15px",
-        border: "1px solid black"
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: props.on ? "white" : "red"
     };
 
     return <div>
