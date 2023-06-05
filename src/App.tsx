a13f0cae0a3484e103764de65d2c5b9fbbf54a0a
Ivan Minets2 <minetssmash@gmail.com>
2023-06-05 16:46:41 +0300

    React memo training

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {ControlledCheckbox, ControlledInput, ControlledSelect} from "./component
 import {UncontrolledInput} from "./components/Inputs/UncontrolledInput";
 import {Select} from "./components/Select/Select";
 import {Select2} from "./components/Select/Select2";
+import {Example1, NewMessagesCounter, Users} from "./testDirectory/ReactMemo";
 
 export function App() {
     let [ratingValue, setRatingValue] = useState<RatingValueType>(0);
@@ -22,35 +23,36 @@ export function App() {
     }
     return (
         <div className={'App'}>
-            <OnOff on={switchOn} onChange={setSwitchOn}/>
-            {/*<PageTitle title = {"Hello!"}/>*/}
-            {/*<Rating value = {3}/>*/}
-            <Accordion titleValue={"Menu1"}
-                       collapsed={accordionCollapsed}
-                       onChange={() => {
-                           setAccordionCollapsed(!accordionCollapsed)
-                       }}
-                       items={[{title: "Ivan1", value: 1}, {title: "Ivan2", value: 2}, {
-                           title: "Ivan3",
-                           value: 3
-                       }, {title: "Ivan4", value: 4}]}
-                       onClick={onClickCallback}
-            />
-            {/*<Accordion titleValue = {"Users123"} collapsed={false}/>*/}
-            <Rating value={ratingValue} onClick={setRatingValue}/>
-            {/*<UncontrolledAccordion titleValue={'--MENU--'}/>*/}
-            {/*<UncontrolledRating />*/}
-            {/*<ControlledInput/>*/}
-            <ControlledSelect/>
-            <Select2
-                value={value}
-                onChange={setValue}
-                items={[
-                {title: "Minsk", value: "1"},
-                {title: "Moscow", value: "2"},
-                {title: "Kiev", value: "3"},
-            ]}/>
+            {/*<OnOff on={switchOn} onChange={setSwitchOn}/>*/}
+            {/*/!*<PageTitle title = {"Hello!"}/>*!/*/}
+            {/*/!*<Rating value = {3}/>*!/*/}
+            {/*<Accordion titleValue={"Menu1"}*/}
+            {/*           collapsed={accordionCollapsed}*/}
+            {/*           onChange={() => {*/}
+            {/*               setAccordionCollapsed(!accordionCollapsed)*/}
+            {/*           }}*/}
+            {/*           items={[{title: "Ivan1", value: 1}, {title: "Ivan2", value: 2}, {*/}
+            {/*               title: "Ivan3",*/}
+            {/*               value: 3*/}
+            {/*           }, {title: "Ivan4", value: 4}]}*/}
+            {/*           onClick={onClickCallback}*/}
+            {/*/>*/}
+            {/*/!*<Accordion titleValue = {"Users123"} collapsed={false}/>*!/*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*/!*<UncontrolledAccordion titleValue={'--MENU--'}/>*!/*/}
+            {/*/!*<UncontrolledRating />*!/*/}
+            {/*/!*<ControlledInput/>*!/*/}
+            {/*<ControlledSelect/>*/}
+            {/*<Select2*/}
+            {/*    value={value}*/}
+            {/*    onChange={setValue}*/}
+            {/*    items={[*/}
+            {/*    {title: "Minsk", value: "1"},*/}
+            {/*    {title: "Moscow", value: "2"},*/}
+            {/*    {title: "Kiev", value: "3"},*/}
+            {/*]}/>*/}
             {/*<ControlledCheckbox/>*/}
+            <Example1/>
         </div>
     );
 }
