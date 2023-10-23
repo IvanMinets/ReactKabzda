f4b9675b30a0b52957c6a9a5ea9a4e268a938c9f
Ivan Minets2 <minetssmash@gmail.com>
2023-10-23 17:19:04 +0300

    src update

--- src/components/UncontrolledAccordion/reducer.test.tsx
+++
@@ -1,9 +0,0 @@
-import {reducer, StateType} from "./reducer";
-
-test('collapsed should be true',()=>{
-    const state: StateType = {
-        collapsed: false
-    }
-    reducer(state, {type:"TOGGLE-COLLAPSED"})
-    expect(state).toBeTruthy();
-})
