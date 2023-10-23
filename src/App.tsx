9819ea0c7e01e729a22eef797cc09982c55b33e0
Ivan Minets2 <minetssmash@gmail.com>
2023-10-23 17:37:39 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,3 +16,14 @@ export function Rating() {
         <div>star</div>
     </div>
 }
+
+export function Accordion(){
+    return <div>
+        <h3>Меню</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+}
