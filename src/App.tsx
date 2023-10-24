b3c4c7ec6964251e90b8c3fcf00848356e9a2e57
Ivan Minets2 <minetssmash@gmail.com>
2023-10-24 14:54:16 +0300

    03, 04 - props

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,24 @@
 import React, {} from 'react';
 import './App.css';
+import {Accordion} from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 export function App() {
     return (
         <div>
-            This is App component
-            <Rating />
-            <Accordion/>
+            <PageTitle title={"This is app component"}/>
+            <PageTitle title={"My Friends"}/>
+            <Rating value={3}/>
+            <Accordion title={"123"}/>
+            <Rating value={4}/>
         </div>
     );
 }
-
-export function Rating() {
-    return <>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-    </>
+type PageTitlePropsType = {
+    title: string
 }
-export function Star ()  {
-    return <div>star</div>
+export function PageTitle(props: PageTitlePropsType) {
+    return <h1>{props.title}</h1>
 }
 
-export function Accordion(){
-    return <div>
-        <AccordionTitle/>
-        <AccordionBody/>
-    </div>
-}
-export function AccordionTitle () {
-    console.log("AccordionTitle rendered")
-    return <>
-        <h3>Меню</h3>
-    </>
-}
-export function AccordionBody () {
-    console.log("AccordionBody rendered")
-    return <>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </>
-}
 
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type AccordionPropsType = {
+    title: string
+}
+
+export function Accordion(props: AccordionPropsType){
+    return <div>
+        <AccordionTitle title ={props.title}/>
+        <AccordionBody/>
+    </div>
+}
+type AccordionTitlePropsType = {
+    title: string
+}
+export function AccordionTitle (props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendered")
+    return <h3>{props.title}</h3>
+}
+export function AccordionBody () {
+    console.log("AccordionBody rendered")
+    return <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+
+}
