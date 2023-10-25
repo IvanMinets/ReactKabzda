309529f24b88841cbea892d3ef06906481cd7e79
Ivan Minets2 <minetssmash@gmail.com>
2023-10-25 18:44:58 +0300

    05 - Rendering, refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import {Rating} from "./components/Rating/Rating";
 export function App() {
     return (
         <div>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
             <Accordion title={"123"} collapsed={true}/>
             <Accordion title={"1234"} collapsed={false}/>
         </div>
